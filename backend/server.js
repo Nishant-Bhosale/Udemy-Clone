911ffc944164a599c0d3c2ffa0f956c8803020e0
Nishant <nishantbhosale244@gmail.com>
2021-09-04 15:42:51 +0530

    CleanUp

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = 5000 || process.env.PORT;
+const app = express();
+
+app.get("/", (req, res) => {
+	res.json({ message: "It's working" });
+});
+
+app.listen(PORT, () => {
+	console.log(`App is running on ${PORT}`);
+});
