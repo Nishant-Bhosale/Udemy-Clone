490e189fddcec64b24a7450cae1311d75f5ed5e7
Nishant <nishantbhosale244@gmail.com>
2021-09-07 17:26:45 +0530

    Added auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,28 @@
-import jsonWebToken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import Student from "../models/Student.js";
+
+const authMiddleware = async (req, res, next) => {
+	const token = req.header("x-auth-token");
+
+	try {
+		const decodedStudent = await jwt.verify(token, process.env.SECRET_KEY);
+
+		const student = await Student.findOne({
+			_id: decodedStudent._id,
+			"tokens.token": token,
+		});
+
+		if (!student) {
+			throw new Error();
+		}
+
+		req.token = token;
+		req.student = student;
+		next();
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ error: "User not found" });
+	}
+};
+
+export default authMiddleware;
