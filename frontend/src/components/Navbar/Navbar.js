abf300d142f0d867b14b702969ed0e01660c6081
AnuragChoudhari <choudharianurag03@gmail.com>
2021-10-17 13:59:28 +0530

    Navbar added

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {Link} from 'react-router-dom'
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Hello !</h1>
+        </div>
+    )
+}
+
+export default Navbar
